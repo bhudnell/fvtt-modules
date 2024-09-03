09cbe9feb6f991933fb5fe76c82ead36f62c97b8
Brendon Hudnell <bthudnell@gmail.com>
2024-09-03 16:31:39 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Modules for Foundry VTT
+
+A collection of modules that I dont want to go through the process of registering with foundry for
+
+## License
+
+All of these software projects are distributed under the [MIT license](./LICENSE).
---
+++ combat-tracker-cr/pf1-combat-tracker-cr.mjs
@@ -0,0 +1,28 @@
+Hooks.on("renderCombatTracker", async (app, html, data) => {
+  let xpTotal = 0;
+  const pcLevels = [];
+  for (const combatant of data.combat.combatants) {
+    if (combatant.actor.type === "npc" && combatant.token.disposition === CONST.TOKEN_DISPOSITIONS.HOSTILE) {
+      xpTotal += combatant.actor.getCRExp(combatant.actor.system.details.cr.total);
+    } else if (
+      combatant.actor.type === "character" &&
+      combatant.token.disposition === CONST.TOKEN_DISPOSITIONS.FRIENDLY
+    ) {
+      pcLevels.push(combatant.actor.system.attributes.hd.total);
+    }
+  }
+
+  const temp = pf1.config.CR_EXP_LEVELS.filter((xp) => xp <= xpTotal);
+  const approxCr = temp.length ? temp.length - 1 : 0;
+
+  let apl = Math.round(pcLevels.reduce((a, b) => a + b) / pcLevels.length);
+  if (pcLevels.length > 5) {
+    apl++;
+  }
+  if (pcLevels.length < 4) {
+    apl--;
+  }
+
+  const header = html.find(".combat-tracker-header");
+  header.append(`<div class="flexrow"><span>Approx. CR: ${approxCr}</span><span>APL: ${apl}</span></div>`);
+});
