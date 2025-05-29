1272ddff9e88730678d51d40c2138d2e92b9afe6
Brendon Hudnell <bthudnell@gmail.com>
2025-05-29 13:27:57 -0600

    added implementation of pf1 item piles that accounts for trade goods and treasure selling at full price

---
+++ pf1-item-piles-tubbz/README.md
@@ -0,0 +1,16 @@
+# PF1e CR in Combat Tracker
+Calculates the approximate CR, APL, and difficulty for actors in the combat encounter
+
+Approximate CR is calculated from NPC, Trap, and Haunt actors with the "Hostile" disposition.
+There is a setting to include actors with the "Secret" disposition
+
+APL is calculated from PC actors with the "Friendly" disposition
+
+Difficulty is defined by table 12-1 [here](https://aonprd.com/Rules.aspx?ID=252) with Trivial added as APL-2
+
+## Manifest: https://raw.githubusercontent.com/bhudnell/fvtt-modules/main/pf1-combat-tracker-cr/module.json
+
+## Known Issues
+
+- CRs of less than 1 will show as 0 instad of 1/2, 1/3, 1/4, etc
+- Changing the token disposition after adding it to the encounter won't update the CR or APL, re-add the token to update
---
+++ pf1-item-piles-tubbz/main.mjs
@@ -0,0 +1,136 @@
+Hooks.once("item-piles-ready", async () => {
+  const VERSIONS = {
+    "11.0": {
+      VERSION: "2.0.0",
+
+      // The actor class type is the type of actor that will be used for the default item pile actor that is created on first item drop.
+      ACTOR_CLASS_TYPE: "npc",
+
+      // The item class type is the type of item that will be used for the default loot item
+      ITEM_CLASS_LOOT_TYPE: "loot",
+
+      // The item class type is the type of item that will be used for the default weapon item
+      ITEM_CLASS_WEAPON_TYPE: "weapon",
+
+      // The item class type is the type of item that will be used for the default equipment item
+      ITEM_CLASS_EQUIPMENT_TYPE: "equipment",
+
+      // The item quantity attribute is the path to the attribute on items that denote how many of that item that exists
+      ITEM_QUANTITY_ATTRIBUTE: "system.quantity",
+
+      // The item price attribute is the path to the attribute on each item that determine how much it costs
+      ITEM_PRICE_ATTRIBUTE: "system.price",
+
+      // This function is an optional system handler that specifically transforms an item's price into a more unified numeric format
+      ITEM_COST_TRANSFORMER: (item, currencies) => {
+        // Account for wand charges, broken condition, and other traits that are not reflected in base price.
+        return item.getValue({ sellValue: 1.0, single: true });
+      },
+
+      // Item filters actively remove items from the item pile inventory UI that users cannot loot, such as spells, feats, and classes
+      ITEM_FILTERS: [
+        {
+          path: "type",
+          filters: "attack,buff,class,feat,race,spell",
+        },
+      ],
+
+      UNSTACKABLE_ITEM_TYPES: ["container"],
+
+      // Item similarities determines how item piles detect similarities and differences in the system
+      ITEM_SIMILARITIES: ["name", "type", "system.subType", "system.masterwork", "system.enh", "system.armor.enh"],
+
+      // Currencies in item piles is a versatile system that can accept actor attributes (a number field on the actor's sheet) or items (actual items in their inventory)
+      // In the case of attributes, the path is relative to the "actor.system"
+      // In the case of items, it is recommended you export the item with `.toObject()` and strip out any module data
+      CURRENCIES: [
+        {
+          type: "attribute",
+          name: "PF1.Currency.Full.pp",
+          img: "systems/pf1/icons/items/inventory/coins-silver.jpg",
+          abbreviation: "{#}P",
+          data: {
+            path: "system.currency.pp",
+          },
+          primary: false,
+          exchangeRate: 10,
+        },
+        {
+          type: "attribute",
+          name: "PF1.Currency.Full.gp",
+          img: "systems/pf1/icons/items/inventory/coin-gold.jpg",
+          abbreviation: "{#}G",
+          data: {
+            path: "system.currency.gp",
+          },
+          primary: true,
+          exchangeRate: 1,
+        },
+        {
+          type: "attribute",
+          name: "PF1.Currency.Full.sp",
+          img: "systems/pf1/icons/items/inventory/coin-silver.jpg",
+          abbreviation: "{#}S",
+          data: {
+            path: "system.currency.sp",
+          },
+          primary: false,
+          exchangeRate: 0.1,
+        },
+        {
+          type: "attribute",
+          name: "PF1.Currency.Full.cp",
+          img: "systems/pf1/icons/items/inventory/coin-copper.jpg",
+          abbreviation: "{#}C",
+          data: {
+            path: "system.currency.cp",
+          },
+          primary: false,
+          exchangeRate: 0.01,
+        },
+      ],
+    },
+  };
+
+  const promises = Object.entries(VERSIONS).map(([version, data]) =>
+    game.itempiles.API.addSystemIntegration(data, version)
+  );
+  await Promise.all(promises);
+});
+
+Hooks.on("item-piles-preOpenInterface", async (merchant, recipient) => {
+  await updateItemPriceModifiers(merchant);
+  if (recipient) {
+    await updateItemPriceModifiers(merchant);
+  }
+});
+
+async function updateItemPriceModifiers(actor) {
+  if (game.itempiles.API.isItemPileMerchant(actor) && !game.user.isGM) {
+    return;
+  }
+
+  // make an updates array
+  const updates = actor.items.reduce((updates, item) => {
+    // Only add the item to the update array if it doesn't already have an sell price modifier
+    // i.e. if the user has configured it, it shouldn't be overwritten
+    if (foundry.utils.hasProperty(item, "flags.item-piles.item.sellPriceModifier")) {
+      return updates;
+    }
+
+    if (!(item.type === "loot" && ["tradeGoods", "treasure"].includes(item.subType))) {
+      return updates;
+    }
+
+    // Add the update to the updates array
+    updates.push({
+      _id: item.id,
+      "flags.item-piles.item.sellPriceModifier": 2,
+    });
+
+    return updates;
+  }, []);
+
+  // Update all items in one go
+  await actor.updateEmbeddedDocuments("Item", updates);
+}
